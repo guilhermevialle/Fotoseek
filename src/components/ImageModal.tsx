bdc4ab3cd1380150d1b60783b0544c35dd107dc7
guilhermevialle <vialleguilherme@gmail.com>
2023-06-07 21:32:06 -0300

    fixing layout

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -55,51 +55,57 @@ export default function ImageModal({ id }: Props) {
 
   return (
     <div className='w-full h-full bg-black bg-opacity-60 z-20 fixed left-0 top-0'>
-      <button
-        className='text-white absolute top-3 left-3'
-        onClick={() => router.push(`${pathname}`)}
-      >
-        <MdClose size={34} />
-      </button>
-      <div className='w-full h-full border-2 flex justify-center items-center'>
-        <Padding paddingY=' py-4 md:py-10 '>
-          <div className='w-full h-full bg-white px-8 rounded shadow-md'>
-            <div className='w-full h-[10%] flex items-center justify-between'>
-              <i onClick={saveLikedImage}>
-                {isLiked(Number(id)) ? (
-                  <IconButton icon={<RxBookmarkFilled size={18} />} />
-                ) : (
-                  <IconButton icon={<RxBookmark size={18} />} />
-                )}
-              </i>
+      <div className='w-full h-full flex justify-center items-center flex-col'>
+        <div className='w-full h-[5%]'>
+          <Padding>
+            <button
+              className='text-white absolute top-3 left-3'
+              onClick={() => router.push(`${pathname}`)}
+            >
+              <MdClose size={34} />
+            </button>
+          </Padding>
+        </div>
+        <div className='w-full h-[95%]'>
+          <Padding paddingY=' py-4 md:py-10 '>
+            <div className='w-full h-full bg-white px-8 rounded shadow-md'>
+              <div className='w-full h-[10%] flex items-center justify-between'>
+                <i onClick={saveLikedImage}>
+                  {isLiked(Number(id)) ? (
+                    <IconButton icon={<RxBookmarkFilled size={18} />} />
+                  ) : (
+                    <IconButton icon={<RxBookmark size={18} />} />
+                  )}
+                </i>
 
-              <Dropdown
-                buttonTitle='Download'
-                sectionTitle='Choose a size'
-                options={dropOptionsArray}
-              />
-            </div>
-            <div className='w-full h-[80%]'>
-              <div
-                className={`w-full h-full bg-neutral-300 rounded shadow-sm overflow-hidden ${
-                  !imageLoadingStatus ? 'animate-pulse' : ''
-                }`}
-              >
-                <img
-                  className='w-full h-full object-cover '
-                  src={image?.src.original}
-                  loading='lazy'
-                  onLoad={() => setImageLoadingStatus(true)}
+                <Dropdown
+                  buttonTitle='Download'
+                  sectionTitle='Choose a size'
+                  options={dropOptionsArray}
                 />
               </div>
+              <div className='w-full h-[80%]'>
+                <div
+                  className={`w-full h-full bg-neutral-300 rounded shadow-sm overflow-hidden ${
+                    !imageLoadingStatus ? 'animate-pulse' : ''
+                  }`}
+                >
+                  <img
+                    className='w-full h-full object-cover '
+                    src={image?.src.original}
+                    loading='lazy'
+                    onLoad={() => setImageLoadingStatus(true)}
+                  />
+                </div>
+              </div>
+              <div className='w-full h-[10%] flex items-center'>
+                <h1 className='text-gray-700 font-semibold'>
+                  {image?.photographer}
+                </h1>
+              </div>
             </div>
-            <div className='w-full h-[10%] flex items-center'>
-              <h1 className='text-gray-700 font-semibold'>
-                {image?.photographer}
-              </h1>
-            </div>
-          </div>
-        </Padding>
+          </Padding>
+        </div>
       </div>
     </div>
   )
